2b14b738b3f79c3ca632896ebe27a4d24e9f1ab7
Ivan Gavrilovich <ivan_gavrilovich@epam.com>
2022-05-22 19:11:34 +0300

    Done package refactoring, fixed minor footer issues, db call issues

--- src/index/features.php
+++ src/index/features.php
@@ -9,7 +9,7 @@ require_once '../database/database.php';
 // }
 
 function get_team($link) {
-    $sql = "SELECT * FROM `employee` JOIN `department`;";
+    $sql = "SELECT * FROM employee LEFT JOIN department ON employee.department_id = department.id;";
     $result = mysqli_query($link, $sql);
     $data = mysqli_fetch_all($result, 1); 
     return $data;
@@ -18,14 +18,14 @@ function get_team($link) {
 $team = get_team($link);
 // $links = get_links($link);
 
-$works_template = file_get_contents("../Templates/works.html");
+$works_template = file_get_contents("../templates/works.html");
 // for ($i = 0; $i < count($links); $i++)
 // {
 //     $works_template = str_replace('{link_'.$i.'}', '"'.$links[$i]['link'].'"', $works_template);
 //     $works_template = str_replace('{works__title_'.$i.'}', $links[$i]['works_title'], $works_template);
 //     $works_template = str_replace('{works__text_'.$i.'}', $links[$i]['works_text'], $works_template);
 // }
-$team_template = file_get_contents("../Templates/team.html");
+$team_template = file_get_contents("../templates/team.html");
 for ($i = 0; $i < count($team); $i++)
 {
     // $team_template = str_replace('{link_'.$i.'}', '"'.$team[$i]['link'].'"', $team_template);
@@ -40,7 +40,7 @@ for ($i = 0; $i < count($team); $i++)
 
 $main_template = file_get_contents("index.html");
 $main_template = str_replace('{team}', $team_template, $main_template);
-$main_template = str_replace('{header}', file_get_contents("../Templates/header.html"), $main_template);
+$main_template = str_replace('{header}', file_get_contents("../templates/header.html"), $main_template);
 $main_template = str_replace('{works}', $works_template, $main_template);
-$main_template = str_replace('{footer}', file_get_contents("../Templates/footer.html"), $main_template);
+$main_template = str_replace('{footer}', file_get_contents("../templates/footer.html"), $main_template);
 echo $main_template;
