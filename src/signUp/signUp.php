3f62259d296915031b565e51505f6ac820433114
VanyaShestakov <vanka1108@gmail.com>
2022-05-23 21:04:34 +0300

    Added checks for existing user

--- src/signUp/signUp.php
+++ src/signUp/signUp.php
@@ -4,7 +4,13 @@ require_once '../database/database.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-echo $username;
+$select_query = 'SELECT * FROM auth_info WHERE username = \''.$username.'\';';
+$result = mysqli_query($link, $select_query);
+$data = mysqli_fetch_all($result, 1); 
 
-$sql = 'INSERT INTO auth_info(username, password) VALUES (\''.$username.'\', \''.$password.'\');';
-mysqli_query($link, $sql);
+if (count($data) != 0) {
+    header("Location: /signUp/logic.php");
+}
+
+$insert_query = 'INSERT INTO auth_info(username, password) VALUES (\''.$username.'\', \''.$password.'\');';
+mysqli_query($link, $insert_query);
