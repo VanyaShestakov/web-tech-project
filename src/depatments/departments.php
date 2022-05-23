aad140889ccc004b024ba5b24daa9c1b479d7315
Ivan Gavrilovich <ivan_gavrilovich@epam.com>
2022-05-23 17:14:38 +0300

    Cleanup

--- src/depatments/departments.php
+++ src/depatments/departments.php
@@ -1,13 +1,6 @@
 <?php
 require_once '../database/database.php';
 
-// function get_links($link) {
-//     $sql = "SELECT * FROM `works_images`;";
-//     $result = mysqli_query($link, $sql);
-//     $data = mysqli_fetch_all($result, 1); 
-//     return $data;
-// }
-
 function get_departments($link) {
     $sql = "SELECT * FROM `department`;";
     $result = mysqli_query($link, $sql);
@@ -16,26 +9,14 @@ function get_departments($link) {
 }
 
 $departments = get_departments($link);
-// $links = get_links($link);
 
 $works_template = file_get_contents("../templates/works.html");
-// for ($i = 0; $i < count($links); $i++)
-// {
-//     $works_template = str_replace('{link_'.$i.'}', '"'.$links[$i]['link'].'"', $works_template);
-//     $works_template = str_replace('{works__title_'.$i.'}', $links[$i]['works_title'], $works_template);
-//     $works_template = str_replace('{works__text_'.$i.'}', $links[$i]['works_text'], $works_template);
-// }
 $team_template = file_get_contents("../templates/team.html");
 for ($i = 0; $i < count($departments); $i++)
 {
-    // $team_template = str_replace('{link_'.$i.'}', '"'.$team[$i]['link'].'"', $team_template);
     $team_template = str_replace('{name_'.$i.'}', $departments[$i]['name'], $team_template);
     $team_template = str_replace('{prof_'.$i.'}', $departments[$i]['description'], $team_template);
     $team_template = str_replace('{text_'.$i.'}', '', $team_template);
-
-    // $team_template = str_replace('{facebook_'.$i.'}', '"'.$team[$i]['link_facebook'].'"', $team_template);
-    // $team_template = str_replace('{inst_'.$i.'}', '"'.$team[$i]['link_inst'].'"', $team_template);
-    // $team_template = str_replace('{linkedIn_'.$i.'}', '"'.$team[$i]['link_linkedIn'].'"', $team_template);
 }
 
 $main_template = file_get_contents("departments.html");
